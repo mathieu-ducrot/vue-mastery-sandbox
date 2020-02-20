c1a92418f432ffd6a243d633943cd91a53683d76
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-02-20 23:11:30 +0100

    Real World Vue.js - first test on vue router using name and redirect

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -7,17 +7,22 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/about-us',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about',
+    redirect: { name: 'about' }
+    // or we can add `alias: '/about'` in the route above
   }
 ]
 
