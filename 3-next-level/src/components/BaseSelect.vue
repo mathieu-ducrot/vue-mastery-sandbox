9d5e5cd9e1183eb71f089f1bb15b36bab2bc1b6c
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-02-29 22:42:50 +0100

    Next Level - Reusable Form Components: BaseSelect

---
+++ 3-next-level/src/components/BaseSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :key="option"
+        :value="option"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSelect',
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      // https://developer.mozilla.org/fr/docs/Web/API/Event/target
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
