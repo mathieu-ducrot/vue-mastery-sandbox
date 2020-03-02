208d8788cb9f04a90d6badcfd69a83f177940e8b
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-02 20:43:44 +0100

    Animating Vue - Transitions

---
+++ 4-animating-vue/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <button @click="toggleModal">Open</button>
+    <!-- named transition -->
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  // https://easings.net/fr
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
