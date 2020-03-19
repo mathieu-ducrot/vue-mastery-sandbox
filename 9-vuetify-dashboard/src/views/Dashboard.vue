345526de24ce93637e1e5952a8ba32c405fb52fc
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-19 13:41:44 +0100

    Vuetify - Challenge Components pt. 2 (use json for data-table items, add multi-sort, more info on the snackbar)

--- 9-vuetify-dashboard/src/views/Dashboard.vue
+++ 9-vuetify-dashboard/src/views/Dashboard.vue
@@ -3,13 +3,15 @@
     <h1>Dashboard</h1>
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="employees"
       :items-per-page="5"
       class="elevation-1"
       @click:row="selectRow"
+      multi-sort
     ></v-data-table>
     <v-snackbar v-model="snackbar">
-      You have selected {{ currentItem }}
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
       <v-btn color="pink" text @click="snackbar = false">
         Close
       </v-btn>
@@ -18,112 +20,42 @@
 </template>
 
 <script>
+import employees from '@/data/employees.json'
+
 export default {
   data() {
     return {
-      currentItem: '',
+      selectedEmployee: {
+        name: '',
+        title: ''
+      },
       snackbar: false,
       headers: [
+        { text: '#', value: 'id' },
         {
-          text: 'Dessert (100g serving)',
+          text: 'Name',
           align: 'start',
-          sortable: false,
           value: 'name'
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
-      ],
-      desserts: [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
+          text: 'Title',
+          value: 'title',
+          sortable: false
         },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        }
+        { text: 'Salary', value: 'salary' }
       ]
     }
   },
   methods: {
     selectRow(event) {
       this.snackbar = true
-      this.currentItem = event.name
+      this.selectedEmployee.name = event.name
+      this.selectedEmployee.title = event.title
+    }
+  },
+  computed: {
+    employees() {
+      return employees
     }
   }
 }
