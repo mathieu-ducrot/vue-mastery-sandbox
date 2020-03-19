a7fa7a5f3aa312ce965b7c4bae5bbb77a760c1e4
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-19 09:53:45 +0100

    Scaling Nuxt - Async/Await & Progress Bar

--- 8-scaling-nuxt/pages/index.vue
+++ 8-scaling-nuxt/pages/index.vue
@@ -17,20 +17,18 @@ export default {
   components: {
     EventCard
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3000/events')
-      .then((reponse) => {
-        return {
-          events: reponse.data
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3000/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again'
       })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. Please try again'
-        })
-      })
+    }
   },
   head() {
     return {
