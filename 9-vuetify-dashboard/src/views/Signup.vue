e8069e4612a63e04cc6de505f0e72fcc08d429ad
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-20 11:23:43 +0100

    Vuetify - Forms: Validation

--- 9-vuetify-dashboard/src/views/Signup.vue
+++ 9-vuetify-dashboard/src/views/Signup.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
           <v-autocomplete
             label="Which browser do you use?"
             :items="browsers"
@@ -16,8 +22,26 @@
             readonly
           ></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn
+            class="mr-4"
+            type="submit"
+            color="primary"
+            :disabled="!formValidity"
+            >Submit</v-btn
+          >
+          <v-btn class="mr-4" color="success" @click="validateForm">
+            Validate Form
+          </v-btn>
+          <v-btn class="mr-4" color="warning" @click="resetValidation">
+            Reset Validation
+          </v-btn>
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -29,8 +53,39 @@ export default {
   name: 'Signup',
   data() {
     return {
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        value =>
+          !!value ||
+          'You must agree to the terms and conditions to sign up for an account'
+      ],
       birthday: '',
-      browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave']
+      browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+      email: '',
+      emailRules: [
+        value => !!value || 'Email is required',
+        value => value.indexOf('@') !== 0 || 'Email should have a username.',
+        value => value.includes('@') || 'Email should include an @ symbol.',
+        value =>
+          value.indexOf('.') - value.indexOf('@') > 1 ||
+          'Email should contain a valid domain.',
+        value => value.includes('.') || 'Email should include a period symbol.',
+        value =>
+          value.indexOf('.') <= value.length - 3 ||
+          'Email should contain a valid domain extension.'
+      ],
+      formValidity: false
+    }
+  },
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset()
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation()
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate()
     }
   }
 }
