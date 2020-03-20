24374abb0b1d26b2b205531119a0d9150cabb5f6
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-20 13:45:46 +0100

    Vue 3 Essentials - Reactive References & Methods

---
+++ 10-vue-3-essentials/src/components/CompositionExample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Capacity: {{ capacity }}</div>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api'
+export default {
+  setup() {
+    const capacity = ref(3)
+
+    function increaseCapacity() {
+      capacity.value++
+    }
+    return { capacity, increaseCapacity }
+  }
+}
+</script>
+
+<style scoped></style>
