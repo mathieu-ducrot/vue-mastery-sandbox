1978ff864253c63d10a8f9b77e0b4393ee7c8b05
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-03-21 09:46:00 +0100

    Vue 3 Essentials - Computed Properties

--- 10-vue-3-essentials/src/components/CompositionExample.vue
+++ 10-vue-3-essentials/src/components/CompositionExample.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
-    <div>Capacity: {{ capacity }}</div>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
     <button @click="increaseCapacity()">Increase Capacity</button>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/composition-api'
+import { ref, computed } from '@vue/composition-api'
 export default {
   setup() {
     const capacity = ref(3)
+    const attending = ref(['Tim', 'Bob', 'Joe'])
+
+    const spacesLeft = computed(() => {
+      return capacity.value - attending.value.length
+    })
 
     function increaseCapacity() {
       capacity.value++
     }
-    return { capacity, increaseCapacity }
+    return { capacity, attending, spacesLeft, increaseCapacity }
   }
 }
 </script>
