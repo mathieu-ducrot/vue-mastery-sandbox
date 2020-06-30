1cdae6229df60c4d15f0381e0596050f4946ae11
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-06-30 09:09:01 +0200

    Vue 3 Gmail Clone - Opening an Email classic Vue2

--- 13_vue3_gmail_clone/src/components/MailTable.vue
+++ 13_vue3_gmail_clone/src/components/MailTable.vue
@@ -4,7 +4,7 @@
     <tr v-for="email in unarchivedEmails"
         :key="email.id"
         :class="['clickable', email.read ? 'read' : '']"
-        @click="readEmail(email)">
+        @click="openEmail(email)">
       <td>
         <input type="checkbox" />
       </td>
@@ -17,11 +17,14 @@
     </tr>
     </tbody>
   </table>
+
+  <MailView v-if="openedEmail" :email="openedEmail" />
 </template>
 
 <script>
   import { format } from 'date-fns';
   import axios from 'axios';
+  import MailView from '@/components/MailView.vue';
 
   export default {
     async setup(){
@@ -29,7 +32,8 @@
       let {data: emails} = await axios.get('http://localhost:3000/emails');
       return {
         format,
-        emails
+        emails,
+        openedEmail: null
       }
     },
     computed: {
@@ -42,10 +46,14 @@
         return this.sortedEmails.filter(e => !e.archived)
       }
     },
+    components: {
+      MailView
+    },
     methods: {
-      readEmail(email){
+      openEmail(email){
         email.read = true
         this.updateEmail(email)
+        this.openedEmail = email;
       },
       archiveEmail(email){
         email.archived = true;
