e446e5f54f4e43bdb28aef98a1fc295e78a90a98
Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
2020-10-13 17:35:43 +0200

    Vue 3 Essentials => Composition API : Modularizing Example

--- 10-vue-3-essentials/src/App.vue
+++ 10-vue-3-essentials/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <CompositionExample />
+    <ModularizingExample />
   </div>
 </template>
 
 <script>
 import CompositionExample from './components/CompositionExample.vue'
+import ModularizingExample from './components/CompositionExample.vue'
 
 export default {
   name: 'App',
   components: {
-    CompositionExample
+    CompositionExample,
+    ModularizingExample
   }
 }
 </script>
---
+++ 10-vue-3-essentials/src/components/ModularizingExample.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script>
+import useEventSpace from '@/use/event-space';
+// import useMapping from "@/use/mapping"; other Composition Function
+
+export default {
+  setup() {
+    const { capacity, attending, spacesLeft, increaseCapacity } = useEventSpace();
+    // const { map, embedId } = useMapping();
+
+    // it's a Vue 3 Best Practice to be explicited with local objects instead of return { ...useEventSpace(), ...useMapping() }
+    return { capacity, attending, spacesLeft, increaseCapacity };
+  }
+}
+
+</script>
+
+<style scoped></style>
