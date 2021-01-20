7e9bb65ebc440f328be7de6391979cce97f8c8b0
Mathieu Ducrot <mathieu.ducrot26@gmail.com>
2021-01-20 08:41:07 +0100

    Touring Vue Router - Programmatic Navigation

--- 15-touring-vue-3-router/src/views/event/Register.vue
+++ 15-touring-vue-3-router/src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+      // If registered then redirect to event details
+
+      this.$router.push({
+        name: 'EventDetails'
+      })
+      // Si on utilise replace à la place de push cela ne va pas pousser une nouvelle entrée dans l'historique du navigateur
+    }
+  }
 }
 </script>
