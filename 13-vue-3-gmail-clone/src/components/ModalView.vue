e5c757c63adb77d148f31255222c0a30329dfac7
Mathieu Ducrot <mathieu.ducrot26@gmail.com>
2021-01-21 08:32:08 +0100

    Gmail Clone - Keyboard Shortcuts Escape

--- 13-vue-3-gmail-clone/src/components/ModalView.vue
+++ 13-vue-3-gmail-clone/src/components/ModalView.vue
@@ -8,11 +8,30 @@
 </template>
 
 <script>
+  import { onBeforeUnmount } from 'vue'
+  
   export default {
     setup(props, {emit}) {
+      let onKeydown = (event) => {
+        console.log(event.key)
+        
+        if(event.key == 'Escape') {
+          emit('closeModal')
+        }
+      }
+
+      window.addEventListener('keydown', onKeydown)
+      onBeforeUnmount(()=>{
+        window.removeEventListener('keydown', onKeydown)
+      })
+
       return {
         emit
       }
     }
   }
 </script>
+
+<style scoped>
+
+</style>
